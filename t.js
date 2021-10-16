a4cf9c7683d26936602179755b695d1a1b39b660
wmat5678 <90573377+wmat5678@users.noreply.github.com>
2021-10-16 21:43:31 +0300

    Create t.js

---
+++ t.js
@@ -0,0 +1,16 @@
+/*
+gjkuiuui
+gjh
+jo*/
+//hil
+
+window.alert("hello");
+console.error("error");
+document.write("jjj");
+console.log("log");
+console.table(["tuui","hkj","t8787"]);
+console.log("fuuy %cuii %cgyuu","color:red;fornt-size:40px");
+function fu () {
+    console.log("hrr");
+ 
+}
